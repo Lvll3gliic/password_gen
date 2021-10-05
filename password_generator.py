67d4cc2700fba3dd8fd08495cd218f67b139c5b0
ee <edgars.eglitis@va.lv>
2021-10-05 23:11:39 +0300

    pievienoju datubazi un zurnalesanu

--- password_generator.py
+++ password_generator.py
@@ -1,10 +1,114 @@
+import logging
+import logging.config
+import mysql.connector
+import requests
+import json
+import yaml
 import string
 import random
+
+from configparser import ConfigParser
+from mysql.connector import Error
+
 # defineju mainigos no kuriem tiks veidotas paroles
 letters = list(string.ascii_letters)
 digits = list(string.digits)
 spec_char = list("!@#$%^&*()_")
 char = list(string.ascii_letters + string.digits + "!@#$%^&*()_")
+
+with open('./log_generator.yaml', 'r') as stream:
+    config = yaml.safe_load(stream)
+
+logging.config.dictConfig(config)
+
+logger = logging.getLogger('root')
+logger.info('Password generator')
+logger.info('Loading configuration from file')
+
+try:
+	config = ConfigParser()
+	config.read('config.ini')
+
+	mysql_config_mysql_host = config.get('mysql_config', 'mysql_host')
+	mysql_config_mysql_db = config.get('mysql_config', 'mysql_db')
+	mysql_config_mysql_user = config.get('mysql_config', 'mysql_user')
+	mysql_config_mysql_pass = config.get('mysql_config', 'mysql_pass')
+
+except:
+	logger.exception('')
+logger.info('DONE')
+
+connection = None
+connected = False
+
+def init_db():
+	global connection
+	connection = mysql.connector.connect(host=mysql_config_mysql_host, database = mysql_config_mysql_db, user = mysql_config_mysql_user, password = mysql_config_mysql_pass)
+
+init_db()
+
+def get_cursor():
+	global connection
+	try:
+		connection.ping(reconnect=True, attempts=1, delay=0)
+		connection.commit()
+	except mysql.connector.Error as err:
+		logger.error("No connection to db " + str(err))
+		connection = init_db()
+		connection.commit()
+	return connection.cursor()
+logger.info('Connecting to MySQL DB')
+try:
+	# connection = mysql.connector.connect(host=mysql_config_mysql_host, database=mysql_config_mysql_db, user=mysql_config_mysql_user, password=mysql_config_mysql_pass)
+	cursor = get_cursor()
+	if connection.is_connected():
+		db_Info = connection.get_server_info()
+		logger.info('Connected to MySQL database. MySQL Server version on ' + str(db_Info))
+		cursor = connection.cursor()
+		cursor.execute("select database();")
+		record = cursor.fetchone()
+		logger.debug('Your connected to - ' + str(record))
+		connection.commit()
+except Error as e :
+	logger.error('Error while connecting to MySQL' + str(e))
+
+# Check if table exists
+def mysql_check_if_password_exists_in_db(password):
+    records = []
+    cursor = get_cursor()
+    try:
+        cursor = connection.cursor()
+        result = cursor.execute("SELECT password FROM used_passwords WHERE password = 'str(password)'")
+        records = cursor.fetchall()
+        connection.commit()
+    except Error as e:
+        logger.error("SELECT password FROM used_passwords WHERE password = 'str(pass)'")
+        logger.error('Problem checking if that password exists: ' + str(e))
+        pass
+    return len(records)
+
+def mysql_insert_password_into_db(password, letters, digits, spec_char, length):
+    cursor = get_cursor()
+    try:
+        cursor = connection.cursor()
+        #result = cursor.execute("INSERT INTO used_passwords(password, letters, digits, spec_char, lenght) VALUES ('str(password)', int(letters), 'int(digits)', 'int(spec_char)', 'int(length)')")
+        #result = cursor.execute("INSERT INTO used_passwords(password, letters, digits, spec_char, lenght) VALUES (?,?,?,?,?)", (str(password), int(letters), int(digits), int(spec_char), int(length)))
+        add_password =("INSERT INTO used_passwords(password, letters, digits, spec_char, lenght) VALUES (%s, %s, %s, %s, %s)")
+        data = (str(password), int(letters), int(digits), int(spec_char), int(length))
+        cursor.execute(add_password, data)
+        connection.commit()
+    except Error as e:
+        logger.error("INSERT INTO used_passwords(password, letters, digits, spec_char, lenght) VALUES ('str(password)', 'int(letters)', 'int(digits)', 'int(spec_char)', 'int(length)')")
+        logger.error('Problem inserting password into db: ' + str(e))
+        pass
+
+def push_password_to_db(password, letters, digits, spec_char, length):
+    if mysql_check_if_password_exists_in_db(password) == 0:
+        logger.debug('Password is not in DB')
+        mysql_insert_password_into_db(password, letters, digits, spec_char, length)
+    else:
+        logger.debug('Password is already in our DB')
+
 # defineju funkciju, kas pieprasis lietotaja datu ievadi un pectam izveidos paroli
 def generate():
          print("This is password generator. You can choose how long password you want, how many letters, digits or special characters you want to have. ")
@@ -51,7 +155,8 @@ def generate():
                   for i in range (length - char_length):
                            password.append(random.choice(char))
          random.shuffle(password)
+         push_password_to_db(password, letter_count, digit_count, spec_char_count, length)
          #tiek konsole izprinteta parole
-         print("".join(password))
+         print( 'Your unique password: ', "".join(password))
 
 generate()
