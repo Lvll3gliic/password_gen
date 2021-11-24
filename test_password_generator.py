49d3385142a83697ab0ec1912087547a36d836c9
ee <edgars.eglitis@va.lv>
2021-11-24 05:09:26 +0200

    pabeigts darbs

--- test_password_generator.py
+++ test_password_generator.py
@@ -3,7 +3,6 @@ from password_generator import *
 print("Password generator test")
 print("-----------")
 print("Testing function /mysql_check_if_password_exists_in_db")
-assert mysql_check_if_password_exists_in_db("parolestests") == 0
-
+assert mysql_check_if_password_exists_in_db("parolestests") == [('parolestessts',)]
 print("OK")
 print("-----------")
